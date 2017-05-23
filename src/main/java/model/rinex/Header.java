a7c4c103dab58756db3bc13f8d31ed11d2191b9a
Bessonov.Evgeniy <Bessonov.Evgeniy@gmail.com>
2017-05-23 23:59:26 +0300

    - Add new Header Labels - Fix State flow pattern

--- src/main/java/model/rinex/Header.java
+++ src/main/java/model/rinex/Header.java
@@ -2,20 +2,31 @@ package model.rinex;
 
 
 import service.observations.rinex.headerLabels.HeaderLabel;
+import service.observations.rinex.headerLabels.MarkerName;
 import service.observations.rinex.headerLabels.PgmRunByDate;
 import service.observations.rinex.headerLabels.RinexVersionType;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Header  {
     HeaderLabel rinexVersionType = new RinexVersionType();
     HeaderLabel pgmRunByDate = new PgmRunByDate();
+    HeaderLabel markerName = new MarkerName();
 
     private List<HeaderLabel> headerLabels = new ArrayList<>();
 
-    public void add(HeaderLabel headerLabel){
-        headerLabels.add(headerLabel);
+    public void set(HeaderLabel headerLabel){
+        if (headerLabel instanceof RinexVersionType) {
+            rinexVersionType = headerLabel;
+        }
+        else if (headerLabel instanceof PgmRunByDate) {
+            pgmRunByDate = headerLabel;
+        }
+        else if (headerLabel instanceof MarkerName) {
+            markerName = headerLabel;
+        }
     }
 
 }
--- src/main/java/service/observations/rinex/HeaderLabelFactory.java
+++ src/main/java/service/observations/rinex/HeaderLabelFactory.java
@@ -1,19 +1,23 @@
 package service.observations.rinex;
 
 import service.observations.rinex.headerLabels.HeaderLabel;
+import service.observations.rinex.headerLabels.MarkerName;
+import service.observations.rinex.headerLabels.PgmRunByDate;
 import service.observations.rinex.headerLabels.RinexVersionType;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class HeaderLabelFactory {
-    HashMap<String, HeaderLabel> headerLabels = new HashMap<>();
+    private HashMap<String, HeaderLabel> headerLabels = new HashMap<>();
 
     HeaderLabelFactory(){
         headerLabels.put("RINEX VERSION / TYPE",   new RinexVersionType());
+        headerLabels.put("PGM / RUN BY / DATE",   new PgmRunByDate());
+        headerLabels.put("MARKER NAME",   new MarkerName());
     }
 
-    HeaderLabel getHeaderLabel(String title) {
+    public HeaderLabel getHeaderLabel(String title) {
         for (Map.Entry<String, HeaderLabel> item: headerLabels.entrySet()){
             String titleItem = item.getKey();
             if (title.contains(titleItem)){
