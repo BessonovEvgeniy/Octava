a89845543ce822561309075c121f2cd751e6005c
Bessonov.Evgeniy <Bessonov.Evgeniy@gmail.com>
2017-07-23 00:14:43 +0300

    Spring settings

--- src/main/java/rinex/service/RinexService.java
+++ src/main/java/rinex/service/RinexService.java
@@ -1,11 +1,12 @@
 package rinex.service;
 
+import org.springframework.stereotype.Service;
 import rinex.model.observations.ReceiverDataModel;
 import org.springframework.beans.factory.annotation.Configurable;
 
 import java.io.InputStream;
 
-@Configurable
+@Service
 public interface RinexService  {
 
     ReceiverDataModel readRinex(InputStream inputStream) throws Exception;
