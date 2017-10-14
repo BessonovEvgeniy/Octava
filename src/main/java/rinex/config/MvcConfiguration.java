45d552b7c32ea05c179902210f009ab9e2003129
Bessonov.Evgeniy <bessonov.evgeniy@gmail.com>
2017-10-14 13:22:12 +0300

    - Fix for upload Rinex files - Small refactoring of class names - Add spring configuration in java class instead xml files

--- src/main/java/rinex/config/MvcConfiguration.java
+++ src/main/java/rinex/config/MvcConfiguration.java
@@ -1,30 +1,31 @@
-//package rinex.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.multipart.MultipartResolver;
-//import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-//import org.springframework.web.servlet.ViewResolver;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//import org.springframework.web.servlet.view.InternalResourceViewResolver;
-//
-//@Configuration
-//@ComponentScan(basePackages = "rinex")
-//@EnableWebMvc
-//public class MvcConfiguration extends WebMvcConfigurerAdapter {
-//
-//    @Bean
-//    public ViewResolver getViewResolver() {
-//        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-//        resolver.setPrefix("/WEB-INF/views/");
-//        resolver.setSuffix(".jsp");
-//        return resolver;
-//    }
-//
-//    @Bean
-//    public MultipartResolver multipartResolver() {
-//        return new StandardServletMultipartResolver();
-//    }
-//}
+package rinex.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = "rinex")
+@EnableWebMvc
+public class MvcConfiguration extends WebMvcConfigurationSupport {
+
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(100000);
+        return new CommonsMultipartResolver();
+    }
+}
