5932fa7d3e8c1ac40d36e525a44ca04fefe56eb0
Bessonov.Evgeniy <bessonov.evgeniy@gmail.com>
2017-10-22 19:55:20 +0300

    - Exception is present javax.servlet.ServletException: Servlet.init() for servlet dispatcher threw exception

--- src/test/java/service/Impl/observations/rinex/RinexServiceImplTest.java
+++ src/test/java/service/Impl/observations/rinex/RinexServiceImplTest.java
@@ -1,23 +1,17 @@
 package service.Impl.observations.rinex;
 
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import rinex.service.RinexService;
-
-import java.io.File;
-
 public class RinexServiceImplTest {
 
 
-    RinexService rinexService;
-
-    @Autowired
-    public RinexServiceImplTest(RinexService rinexServ) {
-        rinexService = rinexServ;
-    }
-
-    @Test
-    public void testReadEmptyFileName() throws Exception {
-        File file = new File("resources/X07100.17o");
-    }
+//    RinexService rinexService;
+//
+//    @Autowired
+//    public RinexServiceImplTest(RinexService rinexServ) {
+//        rinexService = rinexServ;
+//    }
+//
+//    @Test
+//    public void testReadEmptyFileName() throws Exception {
+//        File file = new File("resources/X07100.17o");
+//    }
 }
